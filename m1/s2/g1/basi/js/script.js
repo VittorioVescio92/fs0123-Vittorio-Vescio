6f0253332c5437f4091be69916f457a7e062bd94
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-02-06 16:51:53 +0100

    s2-g1-esercitazione-pomeridiana

---
+++ m1/s2/g1/basi/js/script.js
@@ -0,0 +1,14 @@
+/* commenti su 
+più righe */
+
+//commento a riga singola
+
+//alert("Hello World!"); disabilitato
+
+
+
+/* Gruppo 7
+
+luigi di fraia 
+alice lazzeri
+francesco cacciapuoti
---
+++ m1/s2/g1/esercitazione-pomeridiana/js/naming.js
@@ -0,0 +1,19 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+
+var petPreferito = '<p> Sei un ghiro</p>';
+console.log(petPreferito);
+var PetPreferito = '<p>Sei un ippopotamo</p>';
+console.log(PetPreferito);
+var $pet = '<p>Sei uno struzzo</p>';
+console.log($pet);
+var _pet = '<p>Sei un leone</p>';
+console.log(_pet);
+var _pet2 = '<p> Sei una pantera</p>';
+console.log(_pet2);
+
+
+//var 12 = 'numero';
