80599d6c40f36b3bb1b969b78652371935c38e02
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-02-12 21:15:14 +0100

    nuove-funzionalità

--- m1/s2/g5/progetto-settimanale/js/calcolatrice.js
+++ m1/s2/g5/progetto-settimanale/js/calcolatrice.js
@@ -1,16 +1,40 @@
 
+const display = document.getElementById('display')
+const powerBtn = document.getElementById('power-off')
+const buttons = document.getElementsByTagName('button')
 
-      
+      function power() {
+        display.disabled = false;
+        display.placeholder = '0';
+        for (const button of buttons) {
+          if(button.id !== 'power-off'){
+            button.disabled = false;          
+        }}        
+        powerBtn.removeEventListener("click", power) 
+        powerBtn.addEventListener("click", powerOff) 
+      }
+      function powerOff() {
+        display.disabled = true;
+        display.placeholder = '';
+        for (const button of buttons) {
+          if(button.id !== 'power-off'){
+            button.disabled = true;          
+        }}
+        powerBtn.removeEventListener("click", powerOff)
+        powerBtn.addEventListener("click", power)
+      }
+
       let mostraCalcolatrice = document.querySelector("#mostraCalcolatrice");
 
       mostraCalcolatrice.addEventListener("click", function(){
+        document.getElementById('container').hidden = false
+        document.getElementById('domanda').hidden = true
         return window.alert("Bravissimo, ora puoi usare la calcolatrice!")
       })
-      console.log(mostraCalcolatrice)
       
       function aggiungiSimbolo(elemento) {
         let simbolo = elemento.getAttribute("data-simbolo");
-        console.log(typeof aggiungiSimbolo)
+        
 
         let display = document.querySelector("#display");
         display.value += simbolo;
@@ -19,6 +43,6 @@
       function totale() {
         let display = document.querySelector("#display");
         display.value = eval(display.value);
-        console.log(display)
+
       }
 
